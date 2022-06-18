65b52cc7602986d51eaaecdd88cb57da38dca317
elixian <alphacentorus@gmail.com>
2022-06-18 14:59:58 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <ButtonRipple />
+  </div>
+</template>
+
+<script>
+import ButtonRipple from './components/ButtonRipple.vue';
+
+export default {
+  name: 'App',
+  components: {
+    ButtonRipple,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/ButtonRipple.vue
@@ -0,0 +1,17 @@
+<template>
+  <button><span>test</span></button>
+</template>
+
+<script>
+export default{
+  
+}
+</script>
+
+<style lang="scss">
+button {
+  span {
+    color: red;
+  }
+}
+</style>
