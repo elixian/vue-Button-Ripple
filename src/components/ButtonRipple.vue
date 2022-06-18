0f16432ffe23f0c35f1bdffee594b703a59ed08b
elixian <alphacentorus@gmail.com>
2022-06-18 16:15:48 +0200

    3 files modified

--- src/components/ButtonRipple.vue
+++ src/components/ButtonRipple.vue
@@ -1,17 +1,66 @@
 <template>
-  <button><span>test</span></button>
+  <button class="btn">Valider</button>
 </template>
 
 <script>
-export default{
-  
-}
+export default {
+  mounted() {
+    const btns = document.querySelectorAll('.btn');
+    btns.forEach((btn) => {
+      btn.addEventListener('click', function (e) {
+        let x = e.clientX - e.target.offsetLeft;
+        let y = e.clientY;
+        let ripples = document.createElement('span');
+        ripples.classList.add('ripple');
+        ripples.style.left = `${x}px`;
+        ripples.style.top = `${y}px`;
+        this.appendChild(ripples);
+
+        setTimeout(() => {
+          ripples.remove();
+        }, 1000);
+      });
+    });
+  },
+  setup() {
+    return {};
+  },
+};
 </script>
 
 <style lang="scss">
-button {
-  span {
-    color: red;
+.btn {
+  font-family: 'Poppins', sans-serif;
+  position: relative;
+  font-size: 18px;
+  padding: 10px;
+  min-width: 120px;
+  border-radius: 8px;
+  background: #467ff7;
+  border: none;
+  overflow: hidden;
+  .ripple {
+    position: absolute;
+    display: block;
+    transform: translate(-50%, -50%);
+    pointer-events: none;
+    border-radius: 100%;
+    background: white;
+    animation: ripple-anime 0.8s linear forwards;
+  }
+}
+
+@keyframes ripple-anime {
+  0% {
+    width: 0;
+    height: 0;
+    opacity: 0.5;
+  }
+  100% {
+    width: 400px;
+    height: 400px;
+    filter: blur(6px);
+    opacity: 0;
   }
 }
 </style>
